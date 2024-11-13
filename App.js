354857e5dd2761732bbc5a3be5bd8758df4ef548
charanrajb28 <charanrajb282004@gmail.com>
2024-11-13 20:44:00 +0530

    demo commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Balaji hi</Text>
+      <Text>Demo app</Text>
       <StatusBar style="auto" />
     </View>
   );
